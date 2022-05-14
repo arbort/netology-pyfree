64f0eee1a085bb141d80c4eab6cd9f481eec8d9b
Arsenii Bortnikov <arsbortnikov@gmail.com>
2022-05-14 20:54:31 +0300

    Вернул старую версию

--- pyfree-homework-2.py
+++ pyfree-homework-2.py
@@ -3,28 +3,32 @@ help - напечатать справку по программе.
 add - добавить задачу в список (название задачи запрашиваем у пользователя).
 show - напечатать все добавленные задачи."""
 
-tasks = dict()
-
-def showTasks():
-  print(f'Всего задач {len(tasks)}\n\n')
-  for task, date in tasks.items():
-    print(f'{task}: {date}\n')
+tasks = []
+today = []
+tomorrow = []
+other = []
 
 while True:
-  command = input("Введите команду: ")
-  if command == "help":
-    print(HELP)
-  elif command == "show":
-    showTasks()
-  elif command == "add":
-    task = input("Введите название задачи: ")
-    date = input("Введите дату выполнения задачи: ").lower().lstrip().rstrip()
-    tasks[task] = date
-    print("Задача добавлена")
-  elif command == "exit":
-    break
-  else: 
-    print("Неизвестная команда")
-    break
+ command = input("Введите команду: ")
+ if command == "help":
+   print(HELP)
+ elif command == "show":
+   print(tasks, today, tomorrow, other)
+ elif command == "add":
+   task = input("Введите название задачи: ")
+   tasks.append(task)
+   date = input("Введите дату выполнения задачи: ").lower().lstrip().rstrip()
+   if date == "сегодня":
+     today.append(date)
+   elif date == "завтра":
+     tomorrow.append(date)
+   else:
+     other.append(date)
+   print("Задача добавлена")
+ elif command == "exit":
+   break
+ else: 
+   print("Неизвестная команда")
+   break
 
 print("Спасибо за использование! До свидания!")
